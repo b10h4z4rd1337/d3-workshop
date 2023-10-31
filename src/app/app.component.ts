6ca27ca380aa51e51b7a395dbf453f4af52d9fbd
Mathias Tahas <mathias@tah.as>
2023-10-31 10:46:36 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as data from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'D3-workshop';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+
+  }
 }
